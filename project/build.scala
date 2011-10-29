283982393852a09213ac0524fdb9fff910ca42c5
Josh Suereth <joshua.suereth@gmail.com>
2011-10-29 08:16:36 -0400

    Depeneds on master of git plugin now.

--- project/build.scala
+++ project/build.scala
@@ -2,6 +2,5 @@ import sbt._
 object ghpages extends Build {
   override def projects = Seq(root)
   lazy val root = Project("sbt-ghpages-plugin", file(".")) dependsOn(git)
-  // TODO: change back to joshes repo after a branch-key gets merged
-  lazy val git = RootProject(uri("git://github.com/softprops/sbt-git-plugin.git#branch-key"))
+  lazy val git = RootProject(uri("git://github.com/jsuereth/sbt-git-plugin.git"))
 }
