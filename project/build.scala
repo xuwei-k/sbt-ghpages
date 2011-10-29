17565ce2163e74838b4d4a856c6606789fd6123c
softprops <d.tangren@gmail.com>
2011-10-28 21:59:40 -0400

    dep on repo with branch key support

--- project/build.scala
+++ project/build.scala
@@ -2,5 +2,6 @@ import sbt._
 object ghpages extends Build {
   override def projects = Seq(root)
   lazy val root = Project("sbt-ghpages-plugin", file(".")) dependsOn(git)
-  lazy val git = RootProject(uri("git://github.com/jsuereth/sbt-git-plugin.git"))
+  // TODO: change back to joshes repo after a branch-key gets merged
+  lazy val git = RootProject(uri("git://github.com/softprops/sbt-git-plugin.git#branch-key"))
 }
