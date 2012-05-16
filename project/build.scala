ec3549d6c73ee56cfb082e245b9a9f6b82fa92b3
Josh Suereth <joshua.suereth@gmail.com>
2012-05-16 11:20:45 -0400

    Bumped version and finally moved site plugin into separate repo.

--- project/build.scala
+++ project/build.scala
@@ -5,6 +5,7 @@ object ghpages extends Build {
   lazy val root = Project("sbt-ghpages-plugin", file(".")) settings(
      resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
      resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven",
-     addSbtPlugin("com.jsuereth" % "sbt-git-plugin" % "0.4")
+     addSbtPlugin("com.jsuereth" % "sbt-git-plugin" % "0.4"),
+     addSbtPlugin("com.jsuereth" % "sbt-site-plugin" % "0.4.0")
   )
 }
