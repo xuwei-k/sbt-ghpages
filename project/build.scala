9bf2c3dc8af5ce3995c4d328a1218ac39cb3e292
Josh Suereth <joshua.suereth@gmail.com>
2013-07-03 10:46:42 -0400

    Bump version of dependent plugins we use.

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,7 @@ object ghpages extends Build {
   lazy val root = Project("sbt-ghpages", file(".")) settings(
      resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
      resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven",
-     addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.5.0"),
-     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.6.0")
+     addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.1"),
+     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.6.2")
   )
 }
