9dcf1f60b5f88f69827cdb3bc559eb49ef88b457
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-07-01 11:12:57 +0200

    Upgrade sbt-git, sbt-site and sbt

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,7 @@ object ghpages extends Build {
   lazy val root = Project("sbt-ghpages", file(".")) settings(
      resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
      resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven",
-     addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.1"),
-     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.7.0")
+     addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.4"),
+     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.8.1")
   )
 }
