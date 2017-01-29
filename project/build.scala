c899c9a35e0de6f12f78adee6595ba13e75dc73a
nrinaudo <nicolas.rinaudo@iosquare.com>
2017-01-29 17:07:12 +0100

    Rebase #18.

--- project/build.scala
+++ project/build.scala
@@ -6,6 +6,6 @@ object ghpages extends Build {
      resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
      resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven",
      addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5"),
-     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.8.1")
+     addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.0.0")
   )
 }
